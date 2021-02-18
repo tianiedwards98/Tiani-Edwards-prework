7c6cbd8f97a9bc7a314248153bb3c39d651ea720
tianiedwards98 <tiani.edwards@rutgers.edu>
2021-02-18 08:07:24 -0500

    touch ups

---
+++ JavaScript_Basics_Assessment/activity-2.js
@@ -0,0 +1,5 @@
+const name = prompt ('enter a name');
+const name = [];
+if(name > 4){
+   alert ('Your name is greater than 4 characters') 
+}
---
+++ JavaScript_Basics_Assessment/activity-3.js
@@ -0,0 +1,9 @@
+const studentNameList = ['lisa','ben','alvin'];
+const studentNameList = prompt("Enter a name");
+const studentNameList = prompt("Enter another name");
+const studentNameList = prompt("Enter another name");
+
+ studentNameList.push();
+ console.log(StudentNameList);
+
+
